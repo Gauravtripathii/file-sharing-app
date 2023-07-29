03b3ef80eb28f4cde87a1c0cf8757c1019db5edb
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-29 11:05:52 +0530

    client final

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import { useRef, useState, useEffect } from "react";
+import { uploadFile } from "./services/api";
+
+function App() {
+  const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+  const fileInputRef = useRef();
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  };
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+  return (
+    <div className="app">
+      <div className="file-sharing-container">
+        <h1>Simple File Sharing!</h1>
+        <p>Upload and share the download link.</p>
+        <button onClick={() => onUploadClick()}>Upload</button>
+        <input type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={(event) => setFile(event.target.files[0])}
+        />
+
+        <a href={result} target="_blank">{result}</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
